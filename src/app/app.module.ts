dc75fd6bd8af5996e96da0c1a8d0134196e4b73f
tmucha <tmucha@cyfrowypolsat.pl>
2017-03-01 21:34:14 +0100

    AuthService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import {AppComponent} from './app.component';
+import {SearchComponent} from './search/search.component';
 import {Routes, RouterModule} from "@angular/router";
 import {SPOTIFY_PROVIDERS} from "./services/SpotifyService";
 import {APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "@angular/common";
-import { TrackComponent } from './track/track.component';
-import { ArtistComponent } from './artist/artist.component';
-import { AlbumComponent } from './album/album.component';
+import {TrackComponent} from './track/track.component';
+import {ArtistComponent} from './artist/artist.component';
+import {AlbumComponent} from './album/album.component';
+import {AUTH_PROVIDERS} from "./services/AuthService";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'search', pathMatch: 'full' },
-  { path: 'search', component: SearchComponent },
-  { path: 'artists/:id', component: ArtistComponent },
-  { path: 'tracks/:id', component: TrackComponent },
-  { path: 'albums/:id', component: AlbumComponent },
+  {path: '', redirectTo: 'search', pathMatch: 'full'},
+  {path: 'search', component: SearchComponent},
+  {path: 'artists/:id', component: ArtistComponent},
+  {path: 'tracks/:id', component: TrackComponent},
+  {path: 'albums/:id', component: AlbumComponent},
 ];
 
 @NgModule({
@@ -36,9 +37,11 @@ const routes: Routes = [
   ],
   providers: [
     SPOTIFY_PROVIDERS,
+    AUTH_PROVIDERS,
     {provide: APP_BASE_HREF, useValue: '/'},
     {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
